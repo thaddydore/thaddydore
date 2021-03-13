a901ff647909286b4352e9571fc95718c5507f1f
thaddydore <thaddydore@gmail.com>
2021-03-13 20:33:42 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburgerContainer = document.querySelector('#header__main-nav');
+const hamburger = document.querySelector('.header__main-nav--hamburger');
+const links = document.querySelectorAll('.header__main-nav--links li');
+
+hamburger.addEventListener('click', () => {
+  hamburgerContainer.classList.toggle('clicked');
+  links.forEach(link => link.classList.toggle('fade'));
+})
