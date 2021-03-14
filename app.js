79f54336d53eb01734ffe856d087c3d85b37c269
thaddydore <thaddydore@gmail.com>
2021-03-14 13:49:23 +0100

    wc: integrate emailjs

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const hamburgerContainer = document.querySelector('#header__main-nav');
 const hamburger = document.querySelector('.header__main-nav--hamburger');
 const links = document.querySelectorAll('.header__main-nav--links li');
+const emailField = document.querySelector('.email-address');
+const subjectField = document.querySelector('.email-subject');
+const messageField = document.querySelector('.email-message');
+const contactForm = document.querySelector('.contact__form');
+let subject = "";
+let email = "";
+let message = "";
 
 hamburger.addEventListener('click', () => {
   hamburgerContainer.classList.toggle('clicked');
   links.forEach(link => link.classList.toggle('fade'));
-})
+});
+
+emailField.addEventListener('change', (e) => {
+  email += e.target.value;
+});
+
+subjectField.addEventListener('change', (e) => {
+  subject += e.target.value;
+});
+
+messageField.addEventListener('change', (e) => {
+  message += e.target.value;
+});
+
+contactForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  emailjs.init('user_ve3tnEm07NTG7ofkeRfUf');
+
+  this.subject = subject;
+  this.message = message;
+  this.email = email;
+
+
+  emailjs.sendForm('contact_service', 'contact_form', this)
+    .then(function () {
+      console.log('SUCCESS!');
+      resetFormInputs();
+    }, function (error) {
+      console.log('FAILED...', error);
+    });
+
+}
+
+function resetFormInputs() {
+  subjectField.value = "";
+  emailField.value = "";
+  messageField.value = "";
+}
